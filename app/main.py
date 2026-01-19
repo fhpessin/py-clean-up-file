4119a87f48ef953a8afaf47f571a1fed95b09b00
Felipe Pessin <fhpessin@gmail.com>
2026-01-19 13:53:48 -0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,3 +1,17 @@
+import os
+
+
 class CleanUpFile:
-    # write your code here
-    pass
+    def __init__(self, filename: str) -> None:
+        self.filename = filename
+
+    def __enter__(self):
+        # O método enter inicia o contexto de tempo de execução
+        # Neste caso, apenas retornamos a própria instância
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb) -> None:
+        # O método exit limpa o ambiente ao final do bloco with
+        # Verificamos se o arquivo existe antes de tentar removê-lo
+        if os.path.exists(self.filename):
+            os.remove(self.filename)
